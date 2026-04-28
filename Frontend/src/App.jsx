0a19b307cb0dc072641c8e47e0771fbfbec8b64b
haidy sobhy <haidy@example.com>
2026-04-28 08:10:19 +0300

    category page

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Categories from './pages/Categories';
+ 
+function App() {
+  return (
+    
+        <main className="app-main">
+          <Categories />
+        </main>
+   
+  );
+}
+ 
+export default App;
